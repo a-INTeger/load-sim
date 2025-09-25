88bb6e289aaa1d0fbc9e34ee29e886c77fe88786
a-INTeger <49005281+a-INTeger@users.noreply.github.com>
2025-09-25 11:35:32 +0100

    revamped work

--- dataLoader.py
+++ dataLoader.py
@@ -2,54 +2,53 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os, argparse, sys
+import os, argparse, errno
+from json import load
 
 
-parser = argparse.ArgumentParser(prog="npy-average-delay-generator",
-                                 description="convert a series of csv files to a CCDF plot"
+parser = argparse.ArgumentParser(prog="load-sim-graph-generator",
+                                 description="convert results generated into graphs"
                                  )
 parser.add_argument("filename",
-                     help="the .npy file name where the results are stored must be in the same directory as this script",
+                     help="the .json file name where the results are stored must be in the same directory as this script",
                      type=str)
 args = parser.parse_args()
 
 
-if not os.path.exists(f"./{args.filename}"):
-    sys.exit(f"file {args.filename} does not exist, please run loadsimtestenv.py first to generate results")
+if not os.path.exists(f"./results/{args.filename}"):
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), f"./results/{args.filename}")
 
 # load the data
-data = dict(enumerate(np.load(args.filename, allow_pickle=True).flatten(), 1))[1]
-# pick which algorithms to plot 
-# MUST BE THE SAME AS IN THE DISPATCHER CLASSS
-opNames = ["Random", "JSQ", "JIQ", "Softmin-JSQ", "JSQ(2)", "Softmin-JSQ(2)", "TWF"]
-# opNames = ["Softmin-JSQ", "Softmin-JSQ(2)", "TWF"]
-res = {}
-# convert npys to arrays for plotting
-for opName in opNames:
-    arr = []
-    for runNumber, runResults in data.items():
-        for dispatch, results in runResults.items():
-            if dispatch == opName:
-                arr.append(results)
-    res[opName] = arr
+with open(f"./results/{args.filename}", "r") as f:
+    data = load(f)
 
+df = pd.DataFrame.from_dict(data)
+x_name = "Average Incoming Job Rate Per server (\u03BB)"
+df.index.name=x_name
+df.index = df.index.astype('float64')
+
+
+long_df = (
+    df.reset_index()
+      .melt(id_vars=x_name, var_name="line", value_name="delay")
+      .explode("delay")
+)
+
+long_df[x_name] = long_df[x_name].astype('float64')
+long_df["delay"] = long_df["delay"].astype('float64')
+
+# print(long_df.query("line == 'Random'"))
 
 # set large figure size
 plt.figure(figsize=(12.8, 9.6))
 
-# plot each line 
-for key, arr in res.items():
-    df = pd.DataFrame(arr)
-    df.columns = np.linspace(0.0, 1.0, num=50)[1:]
-    df = df.T
-    df.index.name="Average Incoming Job Rate Per server (\u03BB)"
-    df_melted = df.reset_index().melt(id_vars="Average Incoming Job Rate Per server (\u03BB)", var_name="run", value_name="Average Delay")
 
+# plot each line
+sns.lineplot(data=long_df, x=x_name, y="delay", errorbar=("pi", 25), estimator="mean", hue="line")
 
-    sns.lineplot(data=df_melted, x="Average Incoming Job Rate Per server (\u03BB)", y="Average Delay", label=key)
+plt.show()
+# # title and save the graph
+# plt.title("Average Delay vs. Average Incoming Job Rate Per server (\u03BB)")
+# plt.legend(loc="upper left")
 
-# title and save the graph
-plt.title("Average Delay vs. Average Incoming Job Rate Per server (\u03BB)")
-plt.legend(loc="upper left")
-
-plt.savefig(f"{args.filename[:-4]}.png", dpi=180)
+# plt.savefig(f"a.png", dpi=180)
